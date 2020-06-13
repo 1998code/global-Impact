79067601d19a5fe57fc9bf0fd760717c4f1592e0
1998Code <54872601+19044635S@users.noreply.github.com>
2020-06-13 23:58:32 +0800

    1.0.0

---
+++ plugins/font-awesome.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+// the component
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+import { fab } from '@fortawesome/free-brands-svg-icons'
+
+// icon label name <font-awesome-icon>
+// Vue.component('font-awesome-icon', FontAwesomeIcon)
+// Vue.config.productionTip = false
