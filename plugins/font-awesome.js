3fa23d0529f2bb4db7487d23ddf1c4ed29d89b6c
1998Code <54872601+19044635S@users.noreply.github.com>
2020-06-14 00:21:03 +0800

    remove

--- plugins/font-awesome.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-// the component
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-import { fab } from '@fortawesome/free-brands-svg-icons'
-
-// icon label name <font-awesome-icon>
-// Vue.component('font-awesome-icon', FontAwesomeIcon)
-// Vue.config.productionTip = false
